cc76ecb304f6f9e052ed44e21883b2db10ed2e0f
Gaurav <109735369+gaumrab@users.noreply.github.com>
2024-08-16 17:02:56 +0530

    Update Head.jsx

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import DarkMode from "./DarkMode";
-function Head(){
+function Head({theme,Change}){
     return <>
-     <div className="head">
+     <div className="head" id={`${!theme?"WHITE":""}`}>
         <h6>Onebox</h6>
         <div className="subDiv1">
-        <DarkMode />
-        <select>
+        <DarkMode theme={theme} Change={Change}/>
+        <select style={{marginTop:'5px'}} id={`${!theme?"WHITE":""}`}>
             <option>Tim's Workspace</option>
         </select>
         </div>
